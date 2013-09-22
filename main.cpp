0752d12a8110173afab27ef344268a1ffe744b84
aevri <aevri@server.fake>
2013-09-22 20:30:20 +0100

    Start drawing as a scene by tracing rays

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 
 struct Vec3
 {
+    typedef const Vec3& InParam;
+
     Vec3() {}
 
     Vec3(const float x, const float y, const float z)
@@ -15,12 +17,45 @@ struct Vec3
     float x, y, z;
 };
 
+Vec3 operator+(Vec3::InParam left, Vec3::InParam right) {
+    return Vec3{
+        left.x + right.x,
+        left.y + right.y,
+        left.z + right.z
+    };
+}
+
+Vec3 operator*(Vec3::InParam v, const float scale) {
+    return Vec3 {
+        v.x * scale,
+        v.y * scale,
+        v.z * scale
+    };
+}
+
+float dot(Vec3::InParam left, Vec3::InParam right) {
+    return left.x*right.x + left.y*right.y + left.z*right.z;
+}
+
 // Vec3&& normalize(Vec3&& in) {
 //     return Vec3
 // }
 
-float distance2d(float x, float y) {
+float distance2d(const float x, const float y) {
     return sqrt(x*x + y*y);
+}
+
+float distance3d(const float x, const float y, const float z) {
+    return sqrt(x*x + y*y + z*z);
+}
+
+float length(Vec3::InParam in) {
+    return distance3d(in.x, in.y, in.z);
+}
+
+Vec3 normalised(Vec3::InParam in) {
+    const float scale = 1.0f / length(in);
+    return in * scale;
 }
 
 void colourful_square() {
@@ -38,7 +73,45 @@ void colourful_square() {
     }
 }
 
+Vec3 sample(const Vec3::InParam position, const Vec3::InParam direction) {
+    const Vec3 forward{0.0f, 0.0f, 1.0f};
+    const Vec3 right{1.0f, 0.0f, 0.0f};
+    const Vec3 up{0.0f, 1.0f, 0.0f};
+
+    const float red = 64.0f;
+    const float green = 128.0f;
+    const float blue = 127.0f + 127.0f * dot(direction, up);
+
+    return Vec3 {red, green, blue};
+}
+
+void draw_scene(const int image_width, const int image_height) {
+    const Vec3 camera_forward{0.0f, 0.0f, 1.0f};
+    const Vec3 camera_right{1.0f, 0.0f, 0.0f};
+    const Vec3 camera_up{0.0f, 1.0f, 0.0f};
+    const Vec3 camera_pos{0.0f, 10.0f, 0.0f};
+
+    const int half_image_width = image_width / 2;
+    const int half_image_height = image_height / 2;
+
+    for (int y=image_height-1; y>=0; --y) {
+        for (int x=0; x<image_width; ++x) {
+            const Vec3 pixel_offset = Vec3 {
+                float(x-half_image_width),
+                float(y-half_image_height),
+                0.0f};
+
+            const Vec3 pixel_direction = normalised(
+                camera_forward + pixel_offset);
+
+            const Vec3 colour = sample(camera_pos, pixel_direction);
+
+            printf("%c%c%c", int(colour.x), int(colour.y), int(colour.z));
+        }
+    }
+}
+
 int main() {
     printf("P6 512 512 255 ");
-    colourful_square();
+    draw_scene(512, 512);
 }
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 tracer: main.cpp
-	g++ main.cpp -o tracer -Wall
+	g++ main.cpp -o tracer -Wall --std=c++11
 
 test: tracer
 	./tracer > out.ppm
